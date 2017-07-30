166cc22f9cc0d1004d9d398f5d19c01bf333a32f
pamplonafala <pamplonafala@gmail.com>
2017-07-30 16:12:40 -0300

    Comments

--- index.js
+++ index.js
@@ -5,24 +5,31 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 
+//defines route and sincronizes with directory
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+//defines a port
 http.listen(port, function() {
 	console.log('http://localhost:' + port + '/');
 });
 
+//route handler refactor, sends file to the homepage
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+//handles client connection
 io.on('connection', function(socket) {
 	console.log('User connected.');
 	socket.broadcast.emit('info', 'User connected.');
+	
+	//handles cliend disconnection
 	socket.on('disconnect', function () {
 		console.log('User disconnected.');
 		socket.broadcast.emit('info', 'User disconnected.');
 	});
 	
+	//handles message event
 	socket.on('message', function(msg) {
 		io.emit('message', msg);
 		console.log('msg: ' + msg);
